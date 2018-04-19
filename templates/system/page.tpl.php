0d78f6a8fab51714b6b5b98a662c5b876ff12186
Mikaela McGrath <mikaelamcgrath@hotmail.com>
2018-04-19 16:38:45 +1000

    Wrapping body and completed footer styles

--- templates/system/page.tpl.php
+++ templates/system/page.tpl.php
@@ -78,7 +78,7 @@
   <div class="header">
     <?php if ($logo): ?>
       <a class="logo navbar-btn col-md-4 col-xs-4" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
       </a>
     <?php endif; ?>
 
@@ -121,59 +121,59 @@
     <?php endif; ?>
   </div>
 </header>
+<div class="container-fluid">
+  <div class="main-container <?php print $container_class; ?>">
 
-<div class="main-container <?php print $container_class; ?>">
+    <header role="banner" id="page-header">
+      <?php if (!empty($site_slogan)): ?>
+        <p class="lead"><?php print $site_slogan; ?></p>
+      <?php endif; ?>
 
-  <header role="banner" id="page-header">
-    <?php if (!empty($site_slogan)): ?>
-      <p class="lead"><?php print $site_slogan; ?></p>
-    <?php endif; ?>
+      <?php print render($page['header']); ?>
+    </header> <!-- /#page-header -->
 
-    <?php print render($page['header']); ?>
-  </header> <!-- /#page-header -->
+    <div class="row">
 
-  <div class="row">
+      <?php if (!empty($page['sidebar_first'])): ?>
+        <aside class="col-sm-3" role="complementary">
+          <?php print render($page['sidebar_first']); ?>
+        </aside>  <!-- /#sidebar-first -->
+      <?php endif; ?>
 
-    <?php if (!empty($page['sidebar_first'])): ?>
+      <section<?php print $content_column_class; ?>>
+      <?php if (!empty($page['highlighted'])): ?>
+        <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
+      <?php endif; ?>
+      <?php if (!empty($breadcrumb)): print $breadcrumb;
+      endif;?>
+      <a id="main-content"></a>
+      <?php print render($title_prefix); ?>
+      <?php if (!empty($title)): ?>
+        <h1 class="page-header"><?php print $title; ?></h1>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+      <?php print $messages; ?>
+      <?php if (!empty($tabs)): ?>
+        <?php print render($tabs); ?>
+      <?php endif; ?>
+      <?php if (!empty($page['help'])): ?>
+        <?php print render($page['help']); ?>
+      <?php endif; ?>
+      <?php if (!empty($action_links)): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
+      <?php print render($page['content']); ?>
+    </section>
+
+    <?php if (!empty($page['sidebar_second'])): ?>
       <aside class="col-sm-3" role="complementary">
-        <?php print render($page['sidebar_first']); ?>
-      </aside>  <!-- /#sidebar-first -->
+        <?php print render($page['sidebar_second']); ?>
+      </aside>  <!-- /#sidebar-second -->
     <?php endif; ?>
 
-    <section<?php print $content_column_class; ?>>
-    <?php if (!empty($page['highlighted'])): ?>
-      <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
-    <?php endif; ?>
-    <?php if (!empty($breadcrumb)): print $breadcrumb;
-    endif;?>
-    <a id="main-content"></a>
-    <?php print render($title_prefix); ?>
-    <?php if (!empty($title)): ?>
-      <h1 class="page-header"><?php print $title; ?></h1>
-    <?php endif; ?>
-    <?php print render($title_suffix); ?>
-    <?php print $messages; ?>
-    <?php if (!empty($tabs)): ?>
-      <?php print render($tabs); ?>
-    <?php endif; ?>
-    <?php if (!empty($page['help'])): ?>
-      <?php print render($page['help']); ?>
-    <?php endif; ?>
-    <?php if (!empty($action_links)): ?>
-      <ul class="action-links"><?php print render($action_links); ?></ul>
-    <?php endif; ?>
-    <?php print render($page['content']); ?>
-  </section>
-
-  <?php if (!empty($page['sidebar_second'])): ?>
-    <aside class="col-sm-3" role="complementary">
-      <?php print render($page['sidebar_second']); ?>
-    </aside>  <!-- /#sidebar-second -->
-  <?php endif; ?>
-
+  </div>
 </div>
 </div>
-
 <?php if (!empty($page['footer'])): ?>
   <footer class="footer <?php print $container_class; ?>">
     <?php print render($page['footer']); ?>
