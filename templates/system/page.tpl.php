cbc9a4e12d4c4069f48514dab4f1120d40b0dff9
Mikaela McGrath <mikaelamcgrath@hotmail.com>
2018-05-02 15:30:27 +1000

    Removed breadcrumbs

--- templates/system/page.tpl.php
+++ templates/system/page.tpl.php
@@ -130,7 +130,7 @@
       <?php if (!empty($page['highlighted'])): ?>
         <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
       <?php endif; ?>
-      <?php if (!empty($breadcrumb)): print $breadcrumb;
+      <?php //if (!empty($breadcrumb)): print $breadcrumb;
       endif;?>
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
