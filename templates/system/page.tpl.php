0dd30d0d24b47c182be11e91d396c6fc65385004
Mikaela McGrath <mikaelamcgrath@hotmail.com>
2018-05-12 13:54:38 +1000

    Changed position of full-width in main page template

--- templates/system/page.tpl.php
+++ templates/system/page.tpl.php
@@ -152,13 +152,13 @@
     <?php endif; ?>
 
   </div>
-  
-</div>
-<div class="row">
+  <div class="row">
     <div class="full-width container">
       <?php print render($page['full_width']); ?>
     </div>
   </div>
+</div>
+
 <section role="banner" id="footer-banner" class="row">
     <?php print render($page['footer_banner']); ?>
   </section>
