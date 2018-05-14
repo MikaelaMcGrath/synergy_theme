157cdd411f7fe8f558999776e48d218bae6493fe
Mikaela McGrath <mikaelamcgrath@hotmail.com>
2018-05-14 15:26:49 +1000

    Removed header from page template and added to new template

--- templates/system/page--header.tpl.php
+++ templates/system/page--header.tpl.php
@@ -109,6 +109,11 @@
   </div>
 </header>
 <div class="container-fluid">
+  <header role="banner" id="page-header" class="row">
+    <div class="container">
+      <?php print render($page['header']); ?>
+    </div>
+  </header> <!-- /#page-header -->
   <div class="main-container <?php print $container_class; ?>">
     
 
