d5f8e14386be1ecbe028db1788e18987ede9a3bd
Mikaela McGrath <mikaelamcgrath@hotmail.com>
2018-07-05 14:14:39 +1000

    Added menu drop down functionality back in to site

--- js/synergy.js
+++ js/synergy.js
@@ -15,7 +15,7 @@
     // run test on resize of the window
     jQuery(window).resize(checkSize);
 
-    //jQuery( ".menu.nav .dropdown-menu" ).wrapInner( "<div class='container'></div>" );
+    jQuery( ".menu.nav .dropdown-menu" ).wrapInner( "<div class='container'></div>" );
 
     
 function checkSize(){
@@ -26,15 +26,15 @@ function checkSize(){
 
 
         jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(200);}, function() {
-           //jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(200);  
+           jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(200);  
          
-           //jQuery("body").trigger("click");
+           jQuery("body").trigger("click");
       
         });
 
 
     } else {
-      // jQuery('.mean-nav>.menu-level-1').find('ul > li > a').removeAttr('data-toggle');
+       jQuery('.mean-nav>.menu-level-1').find('ul > li > a').removeAttr('data-toggle');
 
     }
 }
